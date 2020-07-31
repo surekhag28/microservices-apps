38d20c9766e9247fa22c47ff43e5960632998714
surekhagaikwad <Surekha.Gaikwad@anu.edu.au>
2020-07-31 16:01:11 +1000

    microservice using service discovery

--- movie-catalog-service/src/main/java/com/micro/demo/controller/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/micro/demo/controller/MovieCatalogResource.java
@@ -28,10 +28,10 @@ public class MovieCatalogResource {
 	public List<CatalogItem> getCatalog(@PathVariable String userId){
 		
 		
-		UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" +userId, UserRating.class);
+		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" +userId, UserRating.class);
 		
 		return ratings.getUserRating().stream().map(rating -> {
-			Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 			
 			/* Async call using reactive programming
 			Movie movie = webClientBuilder.build()
