3bf90049453a540dc1a56eb76612a91e97842bd9
surekhagaikwad <Surekha.Gaikwad@anu.edu.au>
2020-07-31 14:08:45 +1000

    microservice without service discovery

---
+++ movie-info-service/src/main/java/com/micro/demo/controller/MovieResource.java
@@ -0,0 +1,18 @@
+package com.micro.demo.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.micro.demo.model.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+	@RequestMapping("/{movieId}")
+	public Movie getMovieInfo(@PathVariable String movieId) {
+		
+		return new Movie(movieId, "Test name");
+	}
+}
